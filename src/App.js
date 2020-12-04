80b8bf0335558d7c3eb8cb49e57aa29337ceb7b3
松本一将 <k.matsumoto@benjmain.co.jp>
2020-12-04 16:20:06 +0900

    synchronize-star-count-with-refetch-queris

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ApolloProvider, Mutation, Query } from 'react-apollo';
 import { ADD_STAR, REMOVE_STAR, SEARCH_REPOSITORIES } from './graphql';
 
 const StarButton = props => {
-  const node = props.node
+  const { node, query, first, last, before, after } = props
   const totalCount = node.stargazers.totalCount
   const viewerHasStarred = node.viewerHasStarred
   const starCount = totalCount === 1 ? "1 star" : `${totalCount} starts`
@@ -22,7 +22,18 @@ const StarButton = props => {
     )
   }
   return (
-    <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}>
+    <Mutation
+      mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
+      refetchQueries={ mutationResult => {
+        console.log({mutationResult})
+        return [
+          {
+            query: SEARCH_REPOSITORIES,
+            variables: { query, first, last, before, after }
+          }
+        ]
+      }}
+    >
       {
         addOrRemoveStar => <StarStatus addOrRemoveStar={addOrRemoveStar}/>
       }
@@ -106,7 +117,7 @@ class App extends Component {
                           <li key={node.id}>
                             <a href= {node.url} target=" _blank">{node.name}</a>
                             &nbsp;
-                            <StarButton node={node}/>
+                            <StarButton node={node} {...{query, first, last, after, before}}/>
                           </li>
                         )
                       })
